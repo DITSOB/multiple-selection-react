2fc89b3415bf866a7e513343f3fb85560695a449
DITSOB <melonbread2248@gmail.com>
2024-11-30 19:35:08 +0545

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,21 @@
+const data = [
+
+    {
+        id : '1', 
+        question : "What are accordion Components?",
+        answer : "Arrocdion components are user interface elements used for organizing data."
+    },
+    {
+        id : '2',
+        question : "What are they used for?",
+        answer : "They are commonly employed in various xontexts, including FAQs, product description and so on."
+    },
+    {
+        id : '3',
+        question : "Can I create an accordin components with a different framework?",
+        answer : "Yes of course, it is every possible to create n accodion component with another framework."
+    },
+
+];
+
+export default data;
